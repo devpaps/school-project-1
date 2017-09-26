23792cf403f18995ac3e47d861b934a0798140cf
Lars Jönsson <crymeat@me.com>
2017-09-26 20:31:28 +0200

    Added a buch of stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,42 @@
-let gulp = require('gulp');
-    sass = require('gulp-sass');
+let gulp            = require('gulp');
+    sass            = require('gulp-sass');
+    pug             = require('gulp-pug');
+    autoprefixer    = require('gulp-autoprefixer');
 
 
 
+var autoprefixerOptions = {
+    browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+    };
+    
+    
 
 
 
 
 gulp.task('sass', function(){
-  return gulp.src('./ex-uppgift/sass/styles.sass')
+  return gulp.src('./assets/sass/style.sass')
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
-    .pipe(gulp.dest('./ex-uppgift/css/style.css'))
+    .pipe(gulp.dest('./assets/css'))
 });
+
+gulp.task('pug', function(){
+    return gulp.src('./index.pug')
+      .pipe(pug())
+      .pipe(gulp.dest('./'))
+  });
+
+
+gulp.task('autoprefixer', function(){
+    return gulp.src('./assets/sass/style.sass')
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest('./assets/css'))
+  });
+
+
+
+gulp.task('watch', function(){
+    gulp.watch('./assets/sass/style.sass', ['sass']);
+    gulp.watch('./assets/sass/style.sass', ['autoprefixer']);
+    gulp.watch('./index.pug', ['pug']);
+  });
