a6280c21ac69f16a21fcfd0bc502756099f0c33e
Lars Jönsson <crymeat@me.com>
2017-10-01 11:25:09 +0200

    Edited alot of stuff

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ const gulp            = require('gulp');
 
 
 gulp.task('sass', () => {
-gulp.src('./assets/sass/style.sass')
+gulp.src('./assets/sass/*.sass')
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
     .pipe(gulp.dest('./assets/css/'))
 });
@@ -24,7 +24,7 @@ gulp.src('./*.pug')
 
 
 gulp.task('autoprefix', () =>
-gulp.src('./assets/css/style.css')
+gulp.src('./assets/css/*.css')
     .pipe(autoprefixer({
         browsers: ['last 2 versions'],
         cascade: false
@@ -41,10 +41,10 @@ gulp.task('browserSync', () => {
     });
 });
 
-gulp.task('watch', ['browserSync', 'sass'],() => {
-    gulp.watch('./assets/sass/style.sass', ['sass']);
-    gulp.watch('./assets/sass/style.sass', ['autoprefix']);
+gulp.task('serve', ['browserSync', 'sass'],() => {
+    gulp.watch('./assets/sass/*.sass', ['sass']);
+    gulp.watch('./assets/sass/*.sass', ['autoprefix']);
     gulp.watch('./*.pug', ['pug']);
     gulp.watch('./*.pug', browserSync.reload);
-    gulp.watch('./assets/sass/style.sass', browserSync.reload);
+    gulp.watch('./assets/sass/*.sass', browserSync.reload);
   });
